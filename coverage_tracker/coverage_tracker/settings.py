1b5edb9a8997e013734b4f841b89129f79c25c7b
Kelvin Ndungu Kinyanjui <kelvinking047@gmail.com>
2023-09-05 22:53:26 +0300

    started data cleaning

--- coverage_tracker/coverage_tracker/settings.py
+++ coverage_tracker/coverage_tracker/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'coverage_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'kelvin_kinyanjui',
+        'PASSWORD':'abcd1234',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
